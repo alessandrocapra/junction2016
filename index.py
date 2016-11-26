25a4d4fc515f6283a03ee736e59b0291ded79e98
Martin Borek <martin.borek@aalto.fi>
2016-11-26 18:01:29 +0200

    QR code working

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
-from bottle import Bottle, run, request, template, response
+from bottle import Bottle, run, request, template, response, HTTPResponse
 import requests
 import json
 import qrcode
 from PIL import Image
+from io import BytesIO
 
 
 app = Bottle()
@@ -10,8 +11,6 @@ app = Bottle()
 
 @app.route('/', method='GET')
 def homepage():
-   #img = qrcode.make('TODO add link')
-   #img = qrcode.make('TODO add link')
    return template('index.html', code='', access_token='')
 
 @app.route('/token', method='GET')
@@ -27,5 +26,22 @@ def get_token():
    resp = requests.post('https://test-restgw.transferwise.com/oauth/token',
       data=data, auth=('f272f4a3-ecc1-44fe-b3f4-9a20e9433f4e', '534cda42-719c-4b26-86c2-c96b7cb03437'))
    return template('index.html', access_token=json.loads(resp.text).get('access_token'), code='')
+  
+@app.route('/qr', method='GET')
+def get_image():
+
+   img = qrcode.make(request.query['url'])
+
+   with BytesIO() as output:
+      img.save(output, 'PNG')
+      data = output.getvalue()
+
+   response.set_header('Content-type', 'image/png')
+
+   return data
+
+
+   #response.set_header('content_type', 'image/jpeg')
+   #return img
 
 run(app, host='127.0.0.1', port=8000)
