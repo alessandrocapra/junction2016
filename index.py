f38aa0a2c388cdf97d71521b3d76ef3b976b8a65
Stefano <ssteee@gmail.com>
2016-11-26 14:51:30 +0200

    Fix redirect + return access_token

--- index.py
+++ index.py
@@ -7,8 +7,7 @@ app = Bottle()
 
 @app.route('/', method='GET')
 def homepage():
-   code = request.GET.get('code')
-   return template('index.html', code=code)
+   return template('index.html', code='', access_token='')
 
 
 @app.route('/token', method='GET')
@@ -19,13 +18,11 @@ def get_token():
       'grant_type': 'authorization_code',
       'client_id': 'f272f4a3-ecc1-44fe-b3f4-9a20e9433f4e',
       'code': code,
-      'redirect_uri': 'http://localhost:8000/token'
+      'redirect_uri': 'http://127.0.0.1:8000/token'
    }
-
    resp = requests.post('https://test-restgw.transferwise.com/oauth/token',
       data=data, auth=('f272f4a3-ecc1-44fe-b3f4-9a20e9433f4e', '534cda42-719c-4b26-86c2-c96b7cb03437'))
-   response.content_type = 'application/json'
-   return json.dumps(resp)
+   return template('index.html', access_token=json.loads(resp.text).get('access_token'), code='')
 
 run(app, host='127.0.0.1', port=8000)
 
