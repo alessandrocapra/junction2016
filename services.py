f6f2f6482ffee179ee68b586ece79e44aa3613e4
Stefano <ssteee@gmail.com>
2016-11-28 18:39:49 +0200

    Remove duplicate files

--- services.py
+++
@@ -1,6 +0,0 @@
-def get_auth_headers(access_token):
-    return {
-        'accept': "application/json",
-        'authorization': "Bearer " + access_token,
-        'content-type': "application/json"
-    }
